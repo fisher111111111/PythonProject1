c805b206fc6bc5fa840333bf3823b8a0e6121d70
Алексей _Git <fisher111111111@mail.ru>
2025-07-20 21:30:34 +0300

    исправил стр 6 и 7 chekin на сheckin , тоже самое с checkout

--- src/data_models/project_data.py
+++ src/data_models/project_data.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 
 class BookingCheckDates(BaseModel):
-    chekin: str
-    chekout: str
+    checkin: str
+    checkout: str
 
 class BookingResponseData (BaseModel):
     firstname: str
