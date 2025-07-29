438bee631371cb90a008e2c46f2b60bd02bdd0fd
Алексей _Git <fisher111111111@mail.ru>
2025-07-29 15:23:00 +0300

    item_id на booking_id

--- src/api/api_manager.py
+++ src/api/api_manager.py
@@ -1,3 +1,4 @@
+import  requests
 from src.enums.constant_of_url import ConstURL
 
 class ItemApiClient:
@@ -5,39 +6,47 @@ class ItemApiClient:
         self.auth_session = auth_session
         self.base_url = ConstURL.BASE_URL.value  # Можно также передавать в конструктор, если он может меняться
 
-    def create_item(self, item_data):
+    def create_item(self, booking_data):
         """Отправляет запрос на создание item."""
-        response = self.auth_session.post(f"{self.base_url}/api/v1/items/", json=item_data)
+        response = self.auth_session.post(f"{self.base_url}/booking", json=booking_data)
         # Базовая проверка, что запрос успешен и мы можем парсить JSON
+        print(response.json())
+        print(response.status_code)
         if response.status_code not in (200, 201):
             response.raise_for_status() # Выбросит HTTPError для плохих статусов
         return response.json()
 
-    def get_items(self):
-        """Отправляет запрос на получение списка items."""
-        response = self.auth_session.get(f"{self.base_url}/api/v1/items/")
-        if response.status_code != 200:
-            response.raise_for_status()
-        return response.json()
+auth_session = requests.Session()
+client = ItemApiClient(auth_session)
+client.create_item({"name": "test"})
+print(client)
 
-    def update_item(self, item_id, upd_item_data):
-        """Отправляет запрос на обновление item."""
-        response = self.auth_session.put(f"{self.base_url}/api/v1/items/{item_id}", json=upd_item_data)
-        if response.status_code != 200:
-            response.raise_for_status()
-        return response.json()
-
-    def patch_item(self,item_id, patch_item_data):
-        """Отправляет запрос на частичное обновление item"""
-        response = self.auth_session.patch(f"{self.base_url}/api/v1/items/{item_id}", json=patch_item_data)
-        if response.status_code != 200:
-            response.raise_for_status()
-        return response.json()
-
-    def delete_item(self, item_id):
-        """Отправляет запрос на удаление item."""
-        response = self.auth_session.delete(f"{self.base_url}/api/v1/items/{item_id}")
-        if response.status_code != 200: # В REST API для DELETE часто возвращают 204 No Content или 200 OK
-            response.raise_for_status()
-        # Для DELETE часто нечего возвращать из тела, либо можно вернуть статус-код или сам response
-        return response.status_code
+    #
+    # def get_items(self):
+    #     """Отправляет запрос на получение списка items."""
+    #     response = self.auth_session.get(f"{self.base_url}/api/v1/items/")
+    #     if response.status_code != 200:
+    #         response.raise_for_status()
+    #     return response.json()
+    #
+    # def update_item(self, item_id, upd_item_data):
+    #     """Отправляет запрос на обновление item."""
+    #     response = self.auth_session.put(f"{self.base_url}/api/v1/items/{item_id}", json=upd_item_data)
+    #     if response.status_code != 200:
+    #         response.raise_for_status()
+    #     return response.json()
+    #
+    # def patch_item(self,item_id, patch_item_data):
+    #     """Отправляет запрос на частичное обновление item"""
+    #     response = self.auth_session.patch(f"{self.base_url}/api/v1/items/{item_id}", json=patch_item_data)
+    #     if response.status_code != 200:
+    #         response.raise_for_status()
+    #     return response.json()
+    #
+    # def delete_item(self, item_id):
+    #     """Отправляет запрос на удаление item."""
+    #     response = self.auth_session.delete(f"{self.base_url}/api/v1/items/{item_id}")
+    #     if response.status_code != 200: # В REST API для DELETE часто возвращают 204 No Content или 200 OK
+    #         response.raise_for_status()
+    #     # Для DELETE часто нечего возвращать из тела, либо можно вернуть статус-код или сам response
+    #     return response.status_code
