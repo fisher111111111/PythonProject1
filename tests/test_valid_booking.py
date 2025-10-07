9601a72ff93df1b933a757339fcfb9725185fa1d
Алексей _Git <fisher111111111@mail.ru>
2025-10-07 17:18:06 +0300

    обновление всех модулей, удаление test_valid_booking.py, переименование test_valid_booking_2 в test_valid_booking.py, добавил фабрику url

--- tests/test_valid_booking.py
+++ tests/test_valid_booking.py
@@ -1,99 +1,55 @@
-
-from src.enums.constant_of_url import ConstURL
+from src.utils.urls import URLs
 from src.data_models.project_data import BookingResponseData
 from src.data_models.project_data_all_bookings import Bookings
 from src.utils.project_data_validator import validate_dates
 from src.utils.project_data_all_bookings_validator import validate_booking_list
-import allure
 
 class TestBookings:
 
-    BASE_URL = ConstURL.BASE_URL.value
-
-    @allure.title("Создание, валидация и удаление букинга с проверкой удаления")
-    def test_create_del_check_booking(self, auth_token, booking_data):
+    def test_create_del_check_booking(self, auth_token, fake_booking_data):
         # Create
-        create = auth_token.post(f"{TestBookings.BASE_URL}/booking", json=booking_data)
+        create = auth_token.post(URLs.bookings_endpoint(), json=fake_booking_data)
         assert create.status_code == 200
         booking_id = create.json().get("bookingid")
 
         # Get + Валидировать и данные, и схему
-        response = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        validate_dates (response, model=BookingResponseData, expected_data=booking_data)
+        response = auth_token.get(URLs.booking_endpoint_id(booking_id))
+        assert response.status_code == 200
+        validate_dates (response, model=BookingResponseData, expected_data=fake_booking_data)
 
         # Delete
-        delete = auth_token.delete(f"{TestBookings.BASE_URL}/booking/{booking_id}")
+        delete = auth_token.delete(URLs.booking_endpoint_id(booking_id))
         assert delete.status_code == 201
 
         #Check delete
-        check_delete = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
+        check_delete = auth_token.get(URLs.booking_endpoint_id(booking_id))
         assert check_delete.status_code == 404, f"Букинг с ID {booking_id} не был удален"
 
-    @allure.title("Изменение и валидация букинга")
-    def test_upd_booking(self, auth_token, booking_data, upd_booking_data):
-        # Create
-        create = auth_token.post(f"{TestBookings.BASE_URL}/booking", json=booking_data)
-        assert create.status_code == 200
-        booking_id = create.json().get("bookingid")
-
-        # Преобразуем полученное тело Create для сравнения с телом Update
-        create_data = create.json().get('booking')
-
+    def test_upd_booking(self, auth_token, get_booking_id, upd_booking_data, del_booking_id):
         #Update
-        update = auth_token.put(f"{TestBookings.BASE_URL}/booking/{booking_id}", json=upd_booking_data)
+        update = auth_token.put(URLs.booking_endpoint_id(get_booking_id), json=upd_booking_data)
         assert update.status_code == 200
-        assert update != create_data, f"Тело обновленного букинга не изменилось, что не ожидалось"
 
         # Get + Валидировать и данные, и схему
-        response = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
+        response = auth_token.get(URLs.booking_endpoint_id(get_booking_id))
+        assert response.status_code == 200
         validate_dates(response, model=BookingResponseData, expected_data=upd_booking_data)
 
-        # Delete
-        delete = auth_token.delete(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        assert delete.status_code == 201
-
-        #Check delete
-        check_delete = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        assert check_delete.status_code == 404, f"Букинг с ID {booking_id} не был удален"
-
-    @allure.title("Частичное изменение и валидация букинга")
-    def test_patch_booking(self, auth_token, booking_data, patch_booking_data):
-        # Create
-        create = auth_token.post(f"{TestBookings.BASE_URL}/booking", json=booking_data)
-        assert create.status_code == 200
-        booking_id = create.json().get("bookingid")
+    def test_patch_booking(self, auth_token, get_booking_id, patch_booking_data, del_booking_id):
 
         # Patching
-        patch = auth_token.patch(f"{TestBookings.BASE_URL}/booking/{booking_id}", json=patch_booking_data)
+        patch = auth_token.patch(URLs.booking_endpoint_id(get_booking_id), json=patch_booking_data)
         assert patch.status_code == 200
 
-        # Объединяем прежние данные с изменёнными
-        checking_data = booking_data.copy()
-        checking_data.update(patch_booking_data)
-
         # Get + Валидировать и данные, и схему
-        response = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        validate_dates(response, model=BookingResponseData, expected_data=checking_data)
+        response = auth_token.get(URLs.booking_endpoint_id(get_booking_id))
+        assert response.status_code == 200
+        validate_dates(response, model=BookingResponseData, expected_data=patch_booking_data)
 
-        # Delete
-        delete = auth_token.delete(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        assert delete.status_code == 201
-
-        # Check delete
-        check_delete = auth_token.get(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        assert check_delete.status_code == 404, f"Букинг с ID {booking_id} не был удален"
-
-    @allure.title("Получение и валидация общего списка всех букингов")
-    def test_check_get_all_booking(self, auth_token, booking_data, bookingids):
-        # Create
-        create = auth_token.post(f"{TestBookings.BASE_URL}/booking", json=booking_data)
-        assert create.status_code == 200
-        booking_id = create.json().get("bookingid")
-
+    def test_check_get_all_booking(self, auth_token, get_booking_id, bookingids, del_booking_id):
         # Get_all + Валидировать и данные, и схему
-        response = auth_token.get(f"{TestBookings.BASE_URL}/booking")
+        response = auth_token.get(URLs.bookings_endpoint())
+        assert len(bookingids) > 0, "Список bookings пуст"
+        assert response.status_code == 200
         validate_booking_list (response, model=Bookings, expected_data=bookingids)
 
-        # Delete
-        delete = auth_token.delete(f"{TestBookings.BASE_URL}/booking/{booking_id}")
-        assert delete.status_code == 201
